a817f0b1fccbfbfe5db6247207e872ea29b44584
Chesy <chesy47@gmail.com>
2021-10-11 19:54:08 +0300

    Timer.js --DONE

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
 
-    let modalState = {};
-    let deadline = '2021-10-13';
+    let deadline = '2021-10-12';
     
     const timer = (id, deadline) => {
 
@@ -18,12 +17,10 @@ window.addEventListener('DOMContentLoaded', () => {
             const t = Date.parse(endtime) - Date.parse(new Date()),
                   seconds = Math.floor((t/1000) % 60),
                   minutes = Math.floor((t/1000/60) % 60),
-                  hours = Math.floor((t/(1000 * 60 * 60)) % 24),
-                  days = Math.floor((t/(1000 * 60 * 60 * 24)));
+                  hours = Math.floor((t/(1000 * 60 * 60)) % 24);
     
             return {
                 'total': t,
-                'days': days,
                 'hours': hours,
                 'minutes': minutes,
                 'seconds': seconds
@@ -32,7 +29,6 @@ window.addEventListener('DOMContentLoaded', () => {
     
         const setClock = (selector, endtime) => {
                 const timer = document.querySelector(selector),
-                        days = timer.querySelector('#days'),
                         hours = timer.querySelector('#hours'),
                         minutes = timer.querySelector('#minutes'),
                         seconds = timer.querySelector('#seconds'),
@@ -42,17 +38,14 @@ window.addEventListener('DOMContentLoaded', () => {
     
             function updateClock() { 
                 const t = getTimeRemaining(endtime);
-    
-                days.textContent = addZero(t.days);
                 hours.textContent = addZero(t.hours);
                 minutes.textContent = addZero(t.minutes);
                 seconds.textContent = addZero(t.seconds);
     
                 if(t.total <= 0) {
-                    days.textContent = '00';
                     hours.textContent = '00';
-                    minutes.textContent = '55';
-                    seconds.textContent = '11';
+                    minutes.textContent = '00';
+                    seconds.textContent = '00';
     
                     clearInterval(timeInterval);
                 }
