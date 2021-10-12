b95f3d660a7ceb14b11bcfb9ffd96f4f6a2b0f7b
vladian95 <kvankvan1995@gmail.com>
2021-10-12 11:44:49 +0300

    about pruduct right content

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
+const ratingItemsList = document.querySelectorAll('.rating__item');
+const ratingItemsArray = Array.prototype.slice.call(ratingItemsList);
+
+ratingItemsArray.forEach(item => 
+    item.addEventListener('click', () => {
+        const { itemValue } = item.dataset;
+        item.parentNode.dataset.totalValue = itemValue;
+    })
+);
+
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
 
